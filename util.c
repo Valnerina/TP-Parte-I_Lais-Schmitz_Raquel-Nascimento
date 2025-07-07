7f386c60ea95de0d5ce89b66f355726912c9538f
Valnerina <laisgiuberti@gmail.com>
2025-07-07 20:38:10 -0300

    Update util.c

--- util.c
+++ util.c
@@ -2,11 +2,14 @@
 #include <stddef.h> // ou #include <stdio.h>
 #include <ctype.h>
 
+// Implementa busca sem diferenciar maiúsculas e minúsculas
 char *strcasestr_custom(const char *haystack, const char *needle) {
     if (!*needle)
         return NULL;
 
+    // Percorre cada caractere de 'haystack'
     for (; *haystack; haystack++) {
+        // Compara caractere atual com o início de 'needle' (sem distinguir maiúsculas)
         if (tolower((unsigned char)*haystack) == tolower((unsigned char)*needle)) {
             const char *h = haystack, *n = needle;
             while (*h && *n &&
