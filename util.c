48f13fa052b2b6d8df6914b882a7668aebd5e301
Valnerina <laisgiuberti@gmail.com>
2025-07-07 20:38:47 -0300

    Update util.c

--- util.c
+++ util.c
@@ -12,11 +12,15 @@ char *strcasestr_custom(const char *haystack, const char *needle) {
         // Compara caractere atual com o início de 'needle' (sem distinguir maiúsculas)
         if (tolower((unsigned char)*haystack) == tolower((unsigned char)*needle)) {
             const char *h = haystack, *n = needle;
+
+            // Compara os caracteres seguintes de forma case-insensitive
             while (*h && *n &&
                    (tolower((unsigned char)*h) == tolower((unsigned char)*n))) {
                 h++;
                 n++;
             }
+            
+            // Se chegou ao fim de 'needle', encontrou a substring
             if (!*n)
                 return (char *)haystack;
         }
